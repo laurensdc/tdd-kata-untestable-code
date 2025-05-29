28a32c15f59139ed8805f21f053bf16496acb69e
Laurens DC <coding@laurensdc.com>
2025-05-29 18:27:09 +0200

    ♻️ DI today to daysUntilChristmas

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,8 +3,9 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
+  const today = new Date(new Date().getFullYear(), new Date().getMonth(), new Date().getDate());
   test("characterization test", () => {
-    expect(daysUntilChristmas()).to.equal(210);
-    expect(daysUntilChristmas()).to.be.a("number");
+    expect(daysUntilChristmas(today)).to.equal(210);
+    expect(daysUntilChristmas(today)).to.be.a("number");
   });
 });
