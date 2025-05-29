3a343745d436a5e737ef5622d68664d05a55eec6
Laurens DC <coding@laurensdc.com>
2025-05-29 20:22:47 +0200

    🔥 remove characterization test

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,18 +3,6 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("characterization test", () => {
-    const date = new Date();
-    const today = new Date(
-      date.getFullYear(),
-      date.getMonth(),
-      date.getDate(),
-    )
-
-    expect(daysUntilChristmas(today)).to.equal(210);
-    expect(daysUntilChristmas(today)).to.be.a("number");
-  });
-
   test("day before Christmas", () => {
     const today = new Date(1994, 11, 24)
     expect(daysUntilChristmas(today)).to.equal(1)
