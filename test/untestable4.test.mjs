9a97ceda5cbbfb26c2b6bf98229309454e243b4f
Laurens DC <coding@laurensdc.com>
2025-05-29 21:11:16 +0200

    ✅ add characterization test for change password

--- test/untestable4.test.mjs
+++ test/untestable4.test.mjs
@@ -1,5 +1,6 @@
-import { afterEach, beforeEach, describe, test } from "vitest";
+import { afterEach, beforeEach, describe, test, expect } from "vitest";
 import { PasswordService, PostgresUserDao } from "../src/untestable4.mjs";
+import argon2 from "@node-rs/argon2"
 
 describe("Untestable 4: enterprise application", () => {
   let service;
@@ -11,7 +12,26 @@ describe("Untestable 4: enterprise application", () => {
     PostgresUserDao.getInstance().close();
   });
 
-  test("todo", async () => {
-    // TODO: write proper tests for both PasswordService and PostgresUserDao
+  test("changed password works (characterization test)", async () => {
+    process.env.PGUSER = 'untestable'
+    process.env.PGHOST = 'localhost'
+    process.env.PGDATABASE = 'untestable'
+    process.env.PGPASSWORD = 'secret'
+    process.env.PGPORT = '5432'
+
+    const users = PostgresUserDao.getInstance()
+
+    const passwordHash = await argon2.hash("safe");
+
+    await users.save({ userId: 1, passwordHash })
+
+    // Just to be sure it's saved properly
+    const u = await users.getById(1)
+    expect(u.passwordHash).to.equal(passwordHash)
+
+    await service.changePassword(1, 'safe', 'changed')
+    const updatedU = await users.getById(1)
+    expect(updatedU.passwordHash).not.to.equal(passwordHash)
+    expect(argon2.verifySync(updatedU.passwordHash, 'changed')).to.equal(true)
   });
 });
