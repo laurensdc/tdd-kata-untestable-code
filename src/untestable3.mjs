b1e8a7d1205636eab998d6f7aa1890e0622c2c11
Laurens DC <coding@laurensdc.com>
2025-05-29 20:33:49 +0200

    ♻️ parsePeopleCsv now takes in csvData

--- src/untestable3.mjs
+++ src/untestable3.mjs
@@ -1,8 +1,6 @@
-import { readFile } from "node:fs/promises";
 import { parse } from "csv-parse/sync";
 
-export async function parsePeopleCsv(filePath) {
-  const csvData = await readFile(filePath, { encoding: "utf8" });
+export async function parsePeopleCsv(csvData) {
   const records = parse(csvData, {
     skip_empty_lines: true,
     trim: true,
--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -1,3 +1,4 @@
+import { readFile } from "node:fs/promises";
 import { describe, test } from "vitest";
 import { expect, fail } from "chai";
 import { parsePeopleCsv } from "../src/untestable3.mjs";
@@ -9,7 +10,8 @@ import { parsePeopleCsv } from "../src/untestable3.mjs";
 
 describe("Untestable 3: CSV file parsing", () => {
   test("characterization test", async () => {
-    const got = await parsePeopleCsv("./test/people.csv");
+    const csvData = await readFile('./test/people.csv', { encoding: "utf8" });
+    const got = await parsePeopleCsv(csvData);
     const expected = [
       { firstName: "firstName", lastName: "lastName", gender: "g", age: NaN },
       { firstName: "Loid", lastName: "Forger", gender: "m" },
@@ -18,4 +20,5 @@ describe("Untestable 3: CSV file parsing", () => {
     ];
     expect(got).to.deep.equal(expected);
   });
+
 });
